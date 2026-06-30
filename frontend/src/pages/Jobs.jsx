0b0078b795235136ff277061ec3b324ab8a8a1b9
JayanthR5 <jayanthramanna51@gmail.com>
2026-06-30 19:27:20 +0530

    project update

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -4,21 +4,26 @@ import { Link } from "react-router-dom";
 
 export default function Jobs() {
   const [jobs, setJobs] = useState([]);
+  const [title, setTitle] = useState("");
+const [location, setLocation] = useState("");
+const [jobType, setJobType] = useState("");
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
+ useEffect(() => {
+  fetchJobs();
+}, [title, location, jobType]);
 
   const fetchJobs = async () => {
     try {
       const token = localStorage.getItem("access");
 
-      const response = await api.get("jobs/", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
+      const response = await api.get(
+  `jobs/?title=${title}&location=${location}&job_type=${jobType}`,
+  {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+);
       setJobs(response.data);
     } catch (error) {
       console.log(error);
@@ -31,6 +36,34 @@ export default function Jobs() {
     <div className="container">
 
       <h1 className="title">Available Jobs</h1>
+      <div className="card">
+
+  <input
+    type="text"
+    placeholder="Search by Job Title"
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}
+  />
+
+  <input
+    type="text"
+    placeholder="Location"
+    value={location}
+    onChange={(e) => setLocation(e.target.value)}
+  />
+
+  <select
+    value={jobType}
+    onChange={(e) => setJobType(e.target.value)}
+  >
+    <option value="">All Job Types</option>
+    <option value="FULL_TIME">Full Time</option>
+    <option value="PART_TIME">Part Time</option>
+    <option value="INTERNSHIP">Internship</option>
+    <option value="REMOTE">Remote</option>
+  </select>
+
+</div>
 
       {jobs.length === 0 ? (
         <h3>No Jobs Found</h3>
